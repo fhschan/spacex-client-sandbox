b558c1426d03a0115940b1de3712337e43cfee45
Francis <francischan@gmail.com>
2021-12-03 15:41:08 -0800

    SPC-0000: Added webpack basic config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import "./styles.css";
+import logo from "./react.png";
+
 export function App() {
-  return <h1>TESTING</h1>;
+  return (
+    <>
+      <img src={logo} alt="React" width={100} height={100} />
+      <h1>TESTING</h1>
+    </>
+  );
 }
 
 export default App;
