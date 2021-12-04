223f2cfa3bf3a76b3e3268be8c21f025a758d065
Francis <francischan@gmail.com>
2021-12-03 16:56:22 -0800

    SPC-0000: Husky, lintstaged config setup.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    // "plugin:eslint-comments/recommended",
+    // "prettier/@typescript-eslint",
+    // "plugin:prettier/recommended",
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./styles.css";
-import logo from "./react.png";
+import './styles.css'
+import logo from './react.png'
 
-export function App() {
+export default function App() {
   return (
     <>
       <img src={logo} alt="React" width={100} height={100} />
@@ -9,7 +9,5 @@ export function App() {
         SpaceX demo - {process.env.NODE_ENV} {process.env.name}
       </h1>
     </>
-  );
+  )
 }
-
-export default App;
